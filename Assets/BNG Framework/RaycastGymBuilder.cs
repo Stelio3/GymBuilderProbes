2da0dc7f22d7559f4d546e70751f732bed37c257
Pablo <pablohernandezjimenez@gmail.com>
2021-08-09 16:48:09 +0100

    Created Gym Builder probe project

---
+++ Assets/BNG Framework/RaycastGymBuilder.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastGymBuilder : MonoBehaviour
+{
+    
+    private LineRenderer lineRenderer;
+
+    public LayerMask layerMask;
+    public float maxlength = 10f;
+    public GameObject go;
+
+    void Start(){
+        lineRenderer = GetComponent<LineRenderer>();
+    }
+    void Update()
+    {
+        RaycastHit hit;
+        // Does the ray intersect any objects excluding the player layer
+        if (Physics.Raycast(transform.position, transform.forward, out hit, maxlength, layerMask, QueryTriggerInteraction.Ignore))
+        {
+            if (hit.collider != null)
+            {
+                go.SetActive(true);
+                go.gameObject.transform.position = hit.point;
+
+                lineRenderer.SetPosition(0, Vector3.zero);
+                lineRenderer.SetPosition(1, new Vector3(0, 0, Vector3.Distance(transform.position, hit.point)));
+            }
+            else
+            {
+                go.SetActive(false);
+            }
+        }
+        
+    }
+}
