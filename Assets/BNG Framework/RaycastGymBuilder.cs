de970da406144b76af6dc9c531ef5fbfb2386bf2
Pablo <pablohernandezjimenez@gmail.com>
2021-08-10 09:10:08 +0100

    Added colisions to selected object and changed line renderer material depending on Raycast hit

--- Assets/BNG Framework/RaycastGymBuilder.cs
+++ Assets/BNG Framework/RaycastGymBuilder.cs
@@ -11,27 +11,41 @@ public class RaycastGymBuilder : MonoBehaviour
     public float maxlength = 10f;
     public GameObject go;
 
+    public Material redM, greenM;
+
     void Start(){
         lineRenderer = GetComponent<LineRenderer>();
+
+        if (go.GetComponentInChildren<Rigidbody>() == null)
+        {
+            Rigidbody rbChild = go.transform.GetChild(0).gameObject.AddComponent<Rigidbody>();
+            rbChild.useGravity = false;
+        }
     }
     void Update()
     {
         RaycastHit hit;
-        // Does the ray intersect any objects excluding the player layer
         if (Physics.Raycast(transform.position, transform.forward, out hit, maxlength, layerMask, QueryTriggerInteraction.Ignore))
         {
-            if (hit.collider != null)
+            if (hit.transform.gameObject.tag == "Wall" && go.tag == "Wall" || hit.transform.gameObject.tag == "Roof" && go.tag == "Roof")
             {
-                go.SetActive(true);
-                go.gameObject.transform.position = hit.point;
-
-                lineRenderer.SetPosition(0, Vector3.zero);
-                lineRenderer.SetPosition(1, new Vector3(0, 0, Vector3.Distance(transform.position, hit.point)));
+                lineRenderer.material = greenM;
             }
             else
             {
-                go.SetActive(false);
+                lineRenderer.material = redM;
             }
+            go.GetComponentInChildren<MeshRenderer>().enabled = true;
+            go.gameObject.transform.position = hit.point;
+            lineRenderer.SetPosition(0, Vector3.zero);
+            lineRenderer.SetPosition(1, new Vector3(0, 0, Vector3.Distance(transform.position, go.gameObject.transform.GetChild(0).position)));
+        }
+        else
+        {
+            go.gameObject.transform.GetChild(0).localPosition = Vector3.zero;
+            go.gameObject.transform.GetChild(0).localRotation = Quaternion.identity;
+            go.GetComponentInChildren<MeshRenderer>().enabled = false;
+            lineRenderer.material = redM;
         }
         
     }
